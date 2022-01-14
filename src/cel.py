67cbd3885c67dcf13c1663cd005c6e4a6f2dabdd
gauthier <gauthier@intellium.ai>
2022-01-14 16:21:22 +0000

    redis service

--- src/cel.py
+++ src/cel.py
@@ -2,7 +2,7 @@ from celery import Celery
 
 celery_app = Celery(
     "test_celery",
-    broker="redis://127.0.0.1:6379/0",
-    backend="redis://127.0.0.1:6379/0",
+    broker_url="redis://redis:6379/0",
+    CELERY_RESULT_BACKEND="redis://redis:6379/0",
     include=["tasks"],
 )
