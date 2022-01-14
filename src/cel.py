93748cfab2561daa89eb36fa27c812d4d36e0d87
gauthier <gauthier@intellium.ai>
2022-01-14 18:46:57 +0000

    run on VM

--- src/cel.py
+++ src/cel.py
@@ -2,7 +2,7 @@ from celery import Celery
 
 celery_app = Celery(
     "test_celery",
-    broker_url="redis://redis:6379/0",
-    CELERY_RESULT_BACKEND="redis://redis:6379/0",
+    broker_url="redis://localhost:6379/0",
+    CELERY_RESULT_BACKEND="redis://localhost:6379/0",
     include=["tasks"],
 )
