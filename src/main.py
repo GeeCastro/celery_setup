dea2ca46b0c952976a012d4e1e57f4aaff87dda8
gauthier <gauthier@intellium.ai>
2022-01-17 00:30:45 +0000

    format main prints

--- src/main.py
+++ src/main.py
@@ -1,7 +1,8 @@
 import tasks
 import time
 
-print("starting task")
+print("--- starting task")
 res = tasks.add.delay(1, 2)
+print(f"--- sleep 2 sec")
 time.sleep(2)
-print(res.get())
+print(f"--- task result 1+2={res.get()}")
