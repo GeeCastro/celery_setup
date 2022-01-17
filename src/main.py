e63b2b9446b0ffe456b76c6f040c5dede3818fd0
gauthier <gauthier@intellium.ai>
2022-01-17 01:05:52 +0000

    lsit dir main

--- src/main.py
+++ src/main.py
@@ -1,11 +1,11 @@
-import tasks
-import time
+# import tasks
+# import time
 import os
 
 print("--- list dir")
 os.listdir()
-print("--- starting task")
-res = tasks.add.delay(1, 2)
-print(f"--- sleep 2 sec")
-time.sleep(2)
-print(f"--- task result 1+2={res.get()}")
+# print("--- starting task")
+# res = tasks.add.delay(1, 2)
+# print(f"--- sleep 2 sec")
+# time.sleep(2)
+# print(f"--- task result 1+2={res.get()}")
