34de10cdb61cab61d38798beb315158b090a632a
gauthier <gauthier@intellium.ai>
2022-01-17 02:25:53 +0000

    add multiply

--- src/main.py
+++ src/main.py
@@ -1,8 +1,13 @@
 import tasks
 import time
+from service import Service
 
-print("--- starting task")
-res = tasks.add.delay(1, 2)
-print(f"--- sleep 2 sec")
-time.sleep(2)
-print(f"--- task result 1+2={res.get()}")
+if __name__ == "__main__":
+    print("--- starting tasks")
+    task_add = tasks.add.delay(1, 2)
+    service = Service(2)
+    task_multiply = tasks.multiply.delay(service=service, y=2)
+    print(f"--- sleep 2 sec")
+    time.sleep(2)
+    print(f"--- task add: {task_add.get()}")
+    print(f"--- task multiply: {task_multiply.get()}")
