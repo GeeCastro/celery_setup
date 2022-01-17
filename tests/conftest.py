1088e1f35974c1177cfc2c49414a3dff17ecb658
gauthier <gauthier@intellium.ai>
2022-01-17 11:29:19 +0000

    setup test celery app

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,12 @@
 import pytest
+import os
+
+REDIS_URL = f"redis://{os.environ.get('REDIS_HOST', 'localhost')}:{os.environ.get('REDIS_PORT', '6379')}/0"
 
 
-@pytest.fixture()
-def mail_connection():
-    yield "2"
+@pytest.fixture(scope="session")
+def celery_config():
+    return {
+        "broker_url": REDIS_URL,
+        "result_backend": REDIS_URL,
+    }
