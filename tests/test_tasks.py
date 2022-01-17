4fe87de0b6da33866fb423cc236d26927be40216
gauthier <gauthier@intellium.ai>
2022-01-17 11:29:28 +0000

    add test_multiply

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -5,21 +5,11 @@ from celery.exceptions import Retry
 # for python 2: use mock.patch from `pip install mock`.
 from unittest.mock import patch
 
-from src.tasks import add
+from src.service import Service
+from src import tasks
 
 
-class TestAdd:
-    # @patch("proj.tasks.Product.order")  # < patching Product in module above
-    # def test_success(self, product_order):
-    def test_success(self):
-        add(3, 30.3)
-        # product_order.assert_called_with(3, 30.3)
+def test_multiply(celery_worker):
+    my_service = Service(att1=2)
 
-    @patch("src.tasks.add.retry")
-    def test_failure(self, send_order_retry):
-
-        # Set a side effect on the patched methods
-        # so that they raise the errors we want.
-        send_order_retry.side_effect = Retry()
-
-        add.delay(3, "30.6")
+    assert tasks.multiply.delay(my_service, 3).get(timeout=10) == 6
