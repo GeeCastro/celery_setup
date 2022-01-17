d018c1334b6c628b0fd196877e06253215a8d507
gauthier <gauthier@intellium.ai>
2022-01-17 02:25:38 +0000

    add multiply

--- src/tasks.py
+++ src/tasks.py
@@ -1,7 +1,18 @@
 from cel import celery_app
+from service import Service
+import json
 
 
 @celery_app.task()
 def add(x: int, y: int):
     result = x + y
+    return result
+
+
+@celery_app.task()
+def multiply(service: Service, y: int):
+    print(f"service ({type(service)}): {service}")
+    constructed_service = Service.load_json(service)
+
+    result = constructed_service.multiply(y)
     return result
