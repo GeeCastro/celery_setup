594d17591e2bff93d8e8d0a4ae40648d8a97ada1
gauthier <gauthier@intellium.ai>
2022-01-17 01:16:08 +0000

    main to delay task

--- src/main.py
+++ src/main.py
@@ -1,11 +1,8 @@
-# import tasks
-# import time
-import os
+import tasks
+import time
 
-print("--- list dir")
-os.listdir()
-# print("--- starting task")
-# res = tasks.add.delay(1, 2)
-# print(f"--- sleep 2 sec")
-# time.sleep(2)
-# print(f"--- task result 1+2={res.get()}")
+print("--- starting task")
+res = tasks.add.delay(1, 2)
+print(f"--- sleep 2 sec")
+time.sleep(2)
+print(f"--- task result 1+2={res.get()}")
